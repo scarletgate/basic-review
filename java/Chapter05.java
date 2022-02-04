5550325a7b7f8fb54acb7c163e1c9de5569f9ee9
scarletgate <xiaoyuedu10@gmail.com>
2022-02-04 08:38:11 +0000

    finish_chapter5

---
+++ java/Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 123456789L;
+		System.out.println(l);
+		double d = 3.1415;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+
+		Float f = 3.14f;
+		System.out.println(f);
+		Integer i = 100;
+		System.out.println(i);
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+	}
+}
--- java/chapter04.java
+++ java/chapter04.java
@@ -1,11 +1,11 @@
 
 public class chapter04 {
 	public static void main(String[] args) {
-		String name = "荒井祐太郎";
+		String str = "荒井祐太郎";
 		int number = number = 7;
-		System.out.println(name);
-		name = "一期一会";
-		System.out.println(name);
+		System.out.println(str);
+		str = "一期一会";
+		System.out.println(str);
 		System.out.println(number);
 	}
 }
