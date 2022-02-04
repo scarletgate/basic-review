c3af890122b08afc34c466d25474c591bad2b251
scarletgate <xiaoyuedu10@gmail.com>
2022-02-04 07:02:06 +0000

    finish_chapter4

---
+++ java/chapter04.java
@@ -0,0 +1,11 @@
+
+public class chapter04 {
+	public static void main(String[] args) {
+		String name = "荒井祐太郎";
+		int number = number = 7;
+		System.out.println(name);
+		name = "一期一会";
+		System.out.println(name);
+		System.out.println(number);
+	}
+}
