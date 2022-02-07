42ff8fdb577df8697e1012eeb8a34792f2cb6aaf
scarletgate <xiaoyuedu10@gmail.com>
2022-02-07 01:01:39 +0000

    finish_chapter8

---
+++ java/chapter08/Dowhile01.java
@@ -0,0 +1,9 @@
+public class Dowhile01 {
+	public static void main(String[] args) {
+		int number = 100;
+		do {
+			number *= 2;
+			System.out.println("Dowhile01 = " + number);
+		} while (number < 50);
+	}
+}
